fea21fe2d07cd538d27c5e7b39afd8bf83f21b3e
denev <65989857+deneb0723@users.noreply.github.com>
2021-01-04 02:24:20 +0900

    Router 설정

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ import cookieParser from "cookie-parser";
 import express from "express";
 import helmet from "helmet";
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import routes from "./routes";
 
 const app = express();
 
@@ -13,6 +16,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet({ contentSecurityPolicy: false }));
 app.use(morgan("dev"));
 
-app.use("/", userRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,9 +1,13 @@
 import express from "express";
+import routes from "../routes";
 
 const userRouter = express.Router();
 
-userRouter.get("/", (req, res) => res.send("Home"));
-userRouter.get("/edit", (req, res) => res.send("Edit"));
-userRouter.get("/password", (req, res) => res.send("Password"));
+userRouter.get(routes.users, (req, res) => res.send("Users"));
+userRouter.get(routes.userDetail, (req, res) => res.send("User Detail"));
+userRouter.get(routes.editProfile, (req, res) => res.send("Edit Profile"));
+userRouter.get(routes.changePassword, (req, res) =>
+  res.send("Change Password")
+);
 
 export default userRouter;
