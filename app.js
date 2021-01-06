a9c303f9caa811d722017ac7451373908bf8726b
denev <65989857+deneb0723@users.noreply.github.com>
2021-01-06 17:16:18 +0900

    header, footer partials 구성

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import express from "express";
 import helmet from "helmet";
+import { localMiddleWare } from "./middlewares";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
@@ -16,6 +17,7 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+app.use(localMiddleWare);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
