919949d4bb69f1188432c3de8b44ca54449ab723
denev <65989857+deneb0723@users.noreply.github.com>
2021-01-06 17:48:38 +0900

    join, login 페이지 작성

--- controllers/userContoller.js
+++ controllers/userContoller.js
@@ -1,6 +1,10 @@
-export const join = (req, res) => res.render("join");
-export const login = (req, res) => res.render("login");
-export const logout = (req, res) => res.render("logout");
-export const userDetail = (req, res) => res.render("userDetail");
-export const editProfile = (req, res) => res.render("editProfile");
-export const changePassword = (req, res) => res.render("changePassword");
+export const join = (req, res) => res.render("join", { pageTitle: "가입하기" });
+export const login = (req, res) => res.render("login", { pageTitle: "로그인" });
+export const logout = (req, res) =>
+  res.render("logout", { pageTitle: "로그아웃" });
+export const userDetail = (req, res) =>
+  res.render("userDetail", { pageTitle: "프로필" });
+export const editProfile = (req, res) =>
+  res.render("editProfile", { pageTitle: "프로필 수정" });
+export const changePassword = (req, res) =>
+  res.render("changePassword", { pageTitle: "비밀번호 변경" });
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,6 +1,15 @@
-export const home = (req, res) => res.render("home");
-export const search = (req, res) => res.render("search");
-export const upload = (req, res) => res.render("upload");
-export const videoDetail = (req, res) => res.render("videoDetail");
-export const editVideo = (req, res) => res.render("editVideo");
-export const deleteVideo = (req, res) => res.render("deleteVideo");
+export const home = (req, res) => res.render("home", { pageTitle: "홈" });
+export const search = (req, res) => {
+  const {
+    query: { term },
+  } = req;
+  res.render("search", { pageTitle: "검색", term });
+};
+export const upload = (req, res) =>
+  res.render("upload", { pageTitle: "업로드" });
+export const videoDetail = (req, res) =>
+  res.render("videoDetail", { pageTitle: "비디오" });
+export const editVideo = (req, res) =>
+  res.render("editVideo", { pageTitle: "비디오 수정" });
+export const deleteVideo = (req, res) =>
+  res.render("deleteVideo", { pageTitle: "비디오 삭제" });
