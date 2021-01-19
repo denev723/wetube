316b6e06464079cc5613fe960a853f3eb136a355
denev <65989857+deneb0723@users.noreply.github.com>
2021-01-20 00:54:48 +0900

    mongo db 연결 및 model 생성, upload 기능 추가

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,9 +1,16 @@
-import { videos } from "../db";
 import routes from "../routes";
+import Video from "../models/Video";
 
 //Home
-export const home = (req, res) =>
-  res.render("home", { pageTitle: "홈", videos });
+export const home = async (req, res) => {
+  try {
+    const videos = await Video.find({});
+    res.render("home", { pageTitle: "홈", videos });
+  } catch (error) {
+    console.log(error);
+    res.render("home", { pageTitle: "홈", videos: [] });
+  }
+};
 
 //Search
 export const search = (req, res) => {
@@ -17,12 +24,17 @@ export const search = (req, res) => {
 export const getUpload = (req, res) =>
   res.render("upload", { pageTitle: "업로드" });
 
-export const postUpload = (req, res) => {
+export const postUpload = async (req, res) => {
   const {
-    body: { file, title, description },
+    body: { title, description },
+    file: { path },
   } = req;
-  //비디오 업로드
-  res.redirect(routes.videoDetail(324393));
+  const newVideo = await Video.create({
+    fileUrl: path,
+    title,
+    description,
+  });
+  res.redirect(routes.videoDetail(newVideo.id));
 };
 
 export const videoDetail = (req, res) =>
--- db.js
+++ db.js
@@ -1,50 +1,18 @@
-export const videos = [
-  {
-    id: 324393,
-    title: "Video awesome",
-    description: "This is something I love",
-    views: 24,
-    videoFile: "https://archive.org/details/BigBuckBunny_124",
-    creator: {
-      id: 121212,
-      name: "Nicolas",
-      email: "nico@las.com",
-    },
-  },
-  {
-    id: 1212121,
-    title: "Video super",
-    description: "This is something I love",
-    views: 24,
-    videoFile: "https://archive.org/details/BigBuckBunny_124",
-    creator: {
-      id: 121212,
-      name: "Nicolas",
-      email: "nico@las.com",
-    },
-  },
-  {
-    id: 55555,
-    title: "Video nice",
-    description: "This is something I love",
-    views: 24,
-    videoFile: "https://archive.org/details/BigBuckBunny_124",
-    creator: {
-      id: 121212,
-      name: "Nicolas",
-      email: "nico@las.com",
-    },
-  },
-  {
-    id: 11111,
-    title: "Video perfect",
-    description: "This is something I love",
-    views: 24,
-    videoFile: "https://archive.org/details/BigBuckBunny_124",
-    creator: {
-      id: 121212,
-      name: "Nicolas",
-      email: "nico@las.com",
-    },
-  },
-];
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+import "./models/Video";
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅ DB에 연결되었습니다.");
+const handleError = (error) => console.log(`❌ DB 연결 에러:${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
