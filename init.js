8dd8b3b0a20a3872174263436104b4e596854165
denev <65989857+deneb0723@users.noreply.github.com>
2021-03-06 07:49:07 +0900

    코드 수정

--- init.js
+++ init.js
@@ -6,6 +6,6 @@ dotenv.config();
 const PORT = process.env.PORT || 4000;
 
 const handleListening = () =>
-  console.log(`✅ https://localhost:${PORT} 서버에 연결되었습니다.`);
+  console.log(`✅ https://localhost:${PORT} 서버에 연결되었습니다!!`);
 
 app.listen(PORT, handleListening);
