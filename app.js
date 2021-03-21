0ed2e7009f09603b453bb8f5ee2b53f2ac905242
denev <65989857+deneb0723@users.noreply.github.com>
2021-03-21 17:46:37 +0900

    local 로그인 추가, route 보호 설정

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
+/* eslint-disable quotes */
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import express from "express";
 import helmet from "helmet";
-import { localMiddleWare } from "./middlewares";
 import morgan from "morgan";
+import passport from "passport";
+import session from "express-session";
+import MongoStore from "connect-mongo";
+import mongoose from "mongoose";
+import { localMiddleWare } from "./middlewares";
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import routes from "./routes";
 
+import "./passport";
+
 const app = express();
+
+const CookieStore = MongoStore(session);
 
 app.use(helmet({ contentSecurityPolicy: false }));
 app.set("view engine", "pug");
@@ -19,6 +28,16 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    resave: true,
+    saveUninitialized: false,
+    store: new CookieStore({ mongooseConnection: mongoose.connection }),
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(localMiddleWare);
 
 app.use(routes.home, globalRouter);
--- controllers/userContoller.js
+++ controllers/userContoller.js
@@ -1,31 +1,43 @@
+/* eslint-disable object-curly-newline */
+/* eslint-disable implicit-arrow-linebreak */
+/* eslint-disable quotes */
+import passport from "passport";
 import routes from "../routes";
+import User from "../models/User";
 
 //Join
 export const getJoin = (req, res) =>
   res.render("join", { pageTitle: "가입하기" });
 
-export const postJoin = (req, res) => {
+export const postJoin = async (req, res, next) => {
   const {
     body: { name, email, password, password2 },
   } = req;
   if (password !== password2) {
     res.status(400);
-    res.redirect(routes.join);
+    res.render("join", { pageTitle: "Join" });
   } else {
-    // 가입 진행 후 로그인 상태
-    res.render(routes.home);
+    try {
+      const user = await User({
+        name,
+        email,
+      });
+      await User.register(user, password);
+      next();
+    } catch (error) {
+      console.log(error);
+      res.redirect(routes.home);
+    }
   }
 };
 
 //Login
 export const getLogin = (req, res) =>
   res.render("login", { pageTitle: "로그인" });
-export const postLogin = (req, res) => {
-  const {
-    body: { email, password },
-  } = req;
-  res.redirect(routes.home);
-};
+export const postLogin = passport.authenticate("local", {
+  failureRedirect: routes.login,
+  successRedirect: routes.home,
+});
 
 export const logout = (req, res) => {
   // 로그아웃 후 홈화면으로 이동
